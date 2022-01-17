964cf0e40bfc57456efb944ad77bfa2275524594
Bram Janssen <bram.janssen2@hva.nl>
2022-01-17 13:20:40 +0100

    Made commandHandler

---
+++ src/handlers/commandHandler.js
@@ -0,0 +1,26 @@
+const client = require("../index");
+const {Collection} = require("discord.js");
+const fs = require("fs");
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`../commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
+
+module.exports = async function (interaction) {
+    if (!interaction.isCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({content: 'There was an error while executing this command!', ephemeral: true});
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,35 +1,14 @@
-const fs = require('fs');
-const {Client, Collection, Intents} = require('discord.js');
+const {Client, Intents} = require('discord.js');
 const {BOT_TOKEN} = require('../config.json');
 const client = new Client({intents: [Intents.FLAGS.GUILDS]});
+const commandHandler = require('./handlers/commandHandler')
 
-
-client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.data.name, command);
-}
-
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
-
-    const command = client.commands.get(interaction.commandName);
-
-    if (!command) return;
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        await interaction.reply({content: 'There was an error while executing this command!', ephemeral: true});
-    }
-});
-
+client.on('interactionCreate', commandHandler)
 
 client.once('ready', () => {
     console.log('Bot booted up!');
 });
 
 client.login(BOT_TOKEN);
+
+module.exports = client;
