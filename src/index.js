7831db32ba31ebebdf9d434925cc578773b64664
Bram Janssen <bram.janssen2@hva.nl>
2022-01-17 13:49:29 +0100

    ready eventHandler

--- src/index.js
+++ src/index.js
@@ -2,12 +2,27 @@ const {Client, Intents} = require('discord.js');
 const {BOT_TOKEN} = require('../config.json');
 const client = new Client({intents: [Intents.FLAGS.GUILDS]});
 const commandHandler = require('./handlers/commandHandler')
+const fs = require("fs");
 
-client.on('interactionCreate', commandHandler)
+/**
+ * commandHandler
+ */
+client.on('interactionCreate', commandHandler);
 
-client.once('ready', () => {
-    console.log('Bot booted up!');
-});
+/**
+ * EventHandler
+ */
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
+
 
 client.login(BOT_TOKEN);
 
