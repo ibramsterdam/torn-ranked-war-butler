250b3f53286860487c9c785a0bb7ede6afc57e4a
Bram Janssen <bram.janssen2@hva.nl>
2022-01-19 18:24:08 +0100

    Update warCoundown.js

--- src/commands/war/warCoundown.js
+++ src/commands/war/warCoundown.js
@@ -15,15 +15,60 @@ module.exports = {
     Promise.all([getTornRankedWarInfo()]).then(function (results) {
       //Use randedWarUtil to return information in an array with objects
       const warObject = getMyFactionWarInfo(results[0].data.rankedwars);
-      console.log(warObject);
+      const factionInfo = warObject[Object.keys(warObject)[0]];
+      const warInfo = warObject[Object.keys(warObject)[1]];
+      const startDateTimestamp = new Date(warInfo.start * 1000);
+      const date = new Date(startDateTimestamp);
+      const dateInfo =
+        date.getDate() +
+        '/' +
+        (date.getMonth() + 1) +
+        '/' +
+        date.getFullYear() +
+        ' ' +
+        date.getHours() +
+        ':' +
+        date.getMinutes() +
+        ':' +
+        date.getSeconds() +
+        ' Torn Time';
 
+      //Create message
       const response = new MessageEmbed()
         .setColor('AQUA')
-        .setTitle(`${warObject[0].name}`)
-        .setDescription(
-          `${interaction.member} has set the enemy faction id that is going to be used in the war.`
+        .setTitle(
+          `${factionInfo[Object.keys(factionInfo)[0]].name} vs ${
+            factionInfo[Object.keys(factionInfo)[1]].name
+          }`
         )
-        .addField('Faction Id:', `test`, true);
+        .setDescription(`The war starts at ${dateInfo}`);
+
+      //War has begun!
+      if (Date.now() > startDateTimestamp) {
+        response.setDescription(`The war has begun!`).addFields(
+          {
+            name: `${factionInfo[Object.keys(factionInfo)[0]].name}`,
+            value: `Score: ${factionInfo[Object.keys(factionInfo)[0]].score}
+        Chain: ${factionInfo[Object.keys(factionInfo)[0]].chain}
+        `,
+            inline: true,
+          },
+          {
+            name: `${factionInfo[Object.keys(factionInfo)[1]].name}`,
+            value: `Score: ${factionInfo[Object.keys(factionInfo)[1]].score}
+        Chain: ${factionInfo[Object.keys(factionInfo)[1]].chain}
+        `,
+            inline: true,
+          }
+        );
+      }
+
+      if (warInfo.winner !== 0) {
+        console.log(warObject);
+        response
+          .setDescription(`The war has ended!`)
+          .addField('Winner', 'test');
+      }
 
       //Reply to the discord client
       interaction.reply({
