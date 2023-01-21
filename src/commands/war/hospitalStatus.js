068ebee6ca1f5f82a71953b3bc0c335084e15298
Bram Janssen <ibramsterdam@hotmail.com>
2023-01-21 10:18:50 +0100

    remove promise wrapper

--- src/commands/war/hospitalStatus.js
+++ src/commands/war/hospitalStatus.js
@@ -23,73 +23,72 @@ module.exports = {
     const { options } = interaction;
     const targetFactionId = options.getNumber("factionid");
     const hospitalMap = new Map();
+    const results = await getFaction(targetFactionId);
 
-    Promise.all([getFaction(targetFactionId)]).then(function (results) {
-      if (results[0].data.error) {
-        const err = new EmbedBuilder()
-          .setColor("Aqua")
-          .setTitle(`🏥 No faction found 🏥`);
-        return interaction.reply({
-          embeds: [err],
-          fetchReply: true,
-        });
-      }
-      //Destructure Json to array of faction members
-      const factionInfo = Object.keys(results[0].data);
-      let factionMemberList = undefined;
-      let factionName = undefined;
-      //Iterate over list to find out where members list is located and define variable
-      for (let i = 0; i < factionInfo.length; i++) {
-        if (factionInfo[i] === "members") {
-          factionMemberList = Object.values(Object.values(results[0].data)[i]);
-        }
-
-        if (factionInfo[i] === "name") {
-          factionName = Object.values(results[0].data)[i];
-        }
+    if (results.data.error) {
+      const err = new EmbedBuilder()
+        .setColor("Aqua")
+        .setTitle(`🏥 No faction found 🏥`);
+      return interaction.reply({
+        embeds: [err],
+        fetchReply: true,
+      });
+    }
+    //Destructure Json to array of faction members
+    const factionInfo = Object.keys(results.data);
+    let factionMemberList = undefined;
+    let factionName = undefined;
+    //Iterate over list to find out where members list is located and define variable
+    for (let i = 0; i < factionInfo.length; i++) {
+      if (factionInfo[i] === "members") {
+        factionMemberList = Object.values(Object.values(results.data)[i]);
       }
 
-      const response = new EmbedBuilder().setColor("Aqua").setDescription(
-        `${
-          interaction.member
-        } has asked for the the hospital list <t:${Math.round(
-          Date.now() / 1000
-        )}:R>.
+      if (factionInfo[i] === "name") {
+        factionName = Object.values(results.data)[i];
+      }
+    }
+
+    const response = new EmbedBuilder().setColor("Aqua").setDescription(
+      `${
+        interaction.member
+      } has asked for the the hospital list <t:${Math.round(
+        Date.now() / 1000
+      )}:R>.
 
         **Important:**
 
         *This list does not update on its own when someone takes medication. Also, switch channels if timestamps dont seem to update.*`
-      );
+    );
 
-      //Make map based on if member is in hospital
-      factionMemberList.forEach((factionMember) => {
-        if (factionMember.status.description.includes("In hospital")) {
-          hospitalMap.set(factionMember.name, factionMember.status.until);
-        }
-      });
-
-      //If hospital list is empty
-      if (hospitalMap.size === 0) {
-        response.setTitle(`🏥 No one in hospital of ${factionName} 🏥`);
-      } else {
-        response.setTitle(`🏥 Hospital List of ${factionName} 🏥`);
+    //Make map based on if member is in hospital
+    factionMemberList.forEach((factionMember) => {
+      if (factionMember.status.description.includes("In hospital")) {
+        hospitalMap.set(factionMember.name, factionMember.status.until);
       }
+    });
 
-      //Order list so that earliest to leave hospital is above in message
-      for (const [key, value] of [...hospitalMap.entries()].sort(
-        (a, b) => a[1] - b[1]
-      )) {
-        response.addFields({
-          name: `${key}`,
-          value: `Leaving hospital <t:${value}:R>`,
-        });
-      }
+    //If hospital list is empty
+    if (hospitalMap.size === 0) {
+      response.setTitle(`🏥 No one in hospital of ${factionName} 🏥`);
+    } else {
+      response.setTitle(`🏥 Hospital List of ${factionName} 🏥`);
+    }
 
-      //Reply to the discord client
-      interaction.reply({
-        embeds: [response],
-        fetchReply: true,
+    //Order list so that earliest to leave hospital is above in message
+    for (const [key, value] of [...hospitalMap.entries()].sort(
+      (a, b) => a[1] - b[1]
+    )) {
+      response.addFields({
+        name: `${key}`,
+        value: `Leaving hospital <t:${value}:R>`,
       });
+    }
+
+    //Reply to the discord client
+    interaction.reply({
+      embeds: [response],
+      fetchReply: true,
     });
   },
 };
