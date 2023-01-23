19dfb5ff23c25b3a37f7b16110027392184d965a
Bram Janssen <ibramsterdam@hotmail.com>
2023-01-23 21:23:21 +0100

    cleanup server command

--- src/commands/public/setup.js
+++ src/commands/public/setup.js
@@ -8,6 +8,7 @@ const {
 const {
   getDashboardButtons,
 } = require("../../components/functions/getDashboardButtons");
+const { upsertDiscordServer } = require("../../functions/prisma/discord");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -22,92 +23,63 @@ module.exports = {
     await interaction.deferReply();
 
     const channelList = await interaction.guild.channels.fetch();
-
     const butlerHQ = channelList.find(
       (channel) => channel.name === "Ranked War Butler"
     );
 
-    if (!butlerHQ) {
-      const category = await interaction.guild.channels.create({
-        name: "Ranked War Butler",
-        type: ChannelType.GuildCategory,
-      });
-      const channel = await interaction.guild.channels.create({
-        name: "butler-dashboard",
-        type: ChannelType.GuildText,
-        parent: category.id,
-      });
-
-      const prisma = require("../../index");
-      const guildID = Number(interaction.guildId);
-      let foundServer;
-      let embeds;
-      let buttons;
-
-      try {
-        foundServer = await prisma.discordServer.upsert({
-          where: {
-            guildId: guildID,
-          },
-          update: {},
-          create: {
-            guildId: guildID,
-          },
-          include: {
-            apiKey: true,
-            factions: true,
-          },
-        });
-      } catch (error) {
-        console.log("error", error);
-      }
-
-      if (foundServer) {
-        embeds = new EmbedBuilder()
-          .setTitle("Ranked War Butler")
-          .setDescription(
-            `Buttons enable when you are whitelisted.
-          Whitelisting happends when payment is made`
-          )
-          .addFields({
-            name: "General information",
-            value: `
-            *Whitelist status* : ${foundServer.isWhitelisted ? "🟢" : "🔴"}
-            *ApiKey status* : ${foundServer.apiKey.length} keys connected!
-            *Polling speed* : ${
-              foundServer.apiKey.length * 20 > 100
-                ? 100
-                : foundServer.apiKey.length * 20
-            } requests per minute
-            *Faction tracking status* : tracking ${
-              foundServer.factions.length
-            } factions!
-            `,
-          })
-          .setColor("#00b0f4")
-          .setFooter({
-            text: "Good luck on warring!",
-          })
-          .setTimestamp();
-        buttons = await getDashboardButtons("noMenuType", true, true);
-      } else {
-        embeds = new EmbedBuilder()
-          .setTitle("Ranked War Butler")
-          .setDescription("Something went wrong, please contact the developer");
-        buttons = await getDashboardButtons("noMenuType", true, true);
-      }
-
-      //Reply to the discord client
-      await channel.send({
-        embeds: [embeds],
-        components: [buttons],
-      });
-
-      return interaction.followUp("Setup Successfull");
+    if (butlerHQ) {
+      return interaction.followUp("You have already setup the butler");
     }
 
-    return interaction.followUp("You have already setup the butler");
+    const category = await interaction.guild.channels.create({
+      name: "Ranked War Butler",
+      type: ChannelType.GuildCategory,
+    });
+    const channel = await interaction.guild.channels.create({
+      name: "butler-dashboard",
+      type: ChannelType.GuildText,
+      parent: category.id,
+    });
+
+    const guildID = Number(interaction.guildId);
+    const server = await upsertDiscordServer(guildID);
+
+    const embeds = new EmbedBuilder()
+      .setTitle("Ranked War Butler")
+      .setDescription(
+        `Buttons enable when you are whitelisted.
+          Whitelisting happends when payment is made`
+      )
+      .addFields({
+        name: "General information",
+        value: `
+            *Whitelist status* : ${server.isWhitelisted ? "🟢" : "🔴"}
+            *ApiKey status* : ${server.apiKey.length} keys connected!
+            *Polling speed* : ${
+              server.apiKey.length * 20 > 100 ? 100 : server.apiKey.length * 20
+            } requests per minute
+            *Faction tracking status* : tracking ${
+              server.factions.length
+            } factions!
+            `,
+      })
+      .setColor("#00b0f4")
+      .setFooter({
+        text: "Good luck on warring!",
+      })
+      .setTimestamp();
+    const buttons = await getDashboardButtons(
+      "noMenuType",
+      !server.isWhitelisted,
+      server.apiKey.length === 0
+    );
 
     //Reply to the discord client
+    await channel.send({
+      embeds: [embeds],
+      components: [buttons],
+    });
+
+    return interaction.followUp("Setup Successfull");
   },
 };
--- src/functions/prisma/discord.js
+++ src/functions/prisma/discord.js
@@ -18,6 +18,29 @@ async function getDiscordServer(guildId) {
   }
 }
 
+async function upsertDiscordServer(guildId) {
+  try {
+    const discordServer = await prisma.discordServer.upsert({
+      where: {
+        guildId: guildId,
+      },
+      update: {},
+      create: {
+        guildId: guildId,
+      },
+      include: {
+        apiKey: true,
+        factions: true,
+      },
+    });
+    console.log(discordServer);
+    return discordServer;
+  } catch (error) {
+    console.log("error", error);
+  }
+}
+
 module.exports = {
   getDiscordServer,
+  upsertDiscordServer,
 };
