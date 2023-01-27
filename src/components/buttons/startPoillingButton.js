5e782a38c55cbade65186d92386d0bda9b16e7f9
Bram Janssen <ibramsterdam@hotmail.com>
2023-01-27 23:35:48 +0100

    Polling works up to 25 players

--- src/components/buttons/startPoillingButton.js
+++ src/components/buttons/startPoillingButton.js
@@ -1,98 +1,41 @@
 const { getDiscordServer } = require("../../functions/prisma/discord");
 const { getRandomItemFromArray } = require("../../util/randomItemFromArray");
 const { getFactionFromTornApi } = require("../../util/tornApiUtil");
-const { EmbedBuilder } = require("discord.js");
+const { sendHospitalStatusEmbed } = require("../functions/hospitalStatusEmbed");
+
 module.exports = {
   developer: false,
   data: { name: "dashboard-start-polling" },
   async execute(interaction, client) {
+    await interaction.reply("Started Polling");
     const guildID = BigInt(interaction.guildId);
     const prisma = require("../../index");
-
-    // setInterval(async () => {
     const server = await getDiscordServer(prisma, guildID);
-    const randomApiKeyObject = getRandomItemFromArray(server.apiKeys);
 
-    // Hospital status
-    server.factions.forEach(async (faction) => {
-      await new Promise((r) => setTimeout(r, 4000));
+    setInterval(async () => {
+      // Hospital status
+      for (const faction of server.factions) {
+        // Select a random ApiKey from the list
+        const randomApiKeyObject = getRandomItemFromArray(server.apiKeys);
+        // remove channel messages
+        const channel = await interaction.guild.channels.cache.get(
+          faction.discordChannelId.toString()
+        );
+        const messages = await channel.messages.fetch();
+        await channel.bulkDelete(messages);
 
-      const results = await getFactionFromTornApi(
-        faction.factionId,
-        randomApiKeyObject.value
-      );
+        // fetch faction information
+        const results = await getFactionFromTornApi(
+          faction.factionId,
+          randomApiKeyObject.value
+        );
 
-      if (results.data.error) {
-        const err = new EmbedBuilder()
-          .setColor("Aqua")
-          .setTitle(`🏥 No faction found 🏥`);
-        return await interaction.guild.channels.cache
-          .get(faction.discordChannelId.toString())
-          .send({
-            embeds: [err],
-          });
+        await sendHospitalStatusEmbed(interaction, results, faction);
+
+        await new Promise((resolve) => setTimeout(resolve, 5000));
       }
+    }, 60000);
 
-      const hospitalMap = new Map();
-      //Destructure Json to array of faction members
-      const factionInfo = Object.keys(results.data);
-      let factionMemberList = undefined;
-      let factionName = undefined;
-      //Iterate over list to find out where members list is located and define variable
-      for (let i = 0; i < factionInfo.length; i++) {
-        if (factionInfo[i] === "members") {
-          factionMemberList = Object.values(Object.values(results.data)[i]);
-        }
-
-        if (factionInfo[i] === "name") {
-          factionName = Object.values(results.data)[i];
-        }
-      }
-
-      const response = new EmbedBuilder().setColor("Aqua").setDescription(
-        `${
-          interaction.member
-        } has asked for the the hospital list <t:${Math.round(
-          Date.now() / 1000
-        )}:R>.
-
-        **Important:**
-
-        *This list does not update on its own when someone takes medication. Also, switch channels if timestamps dont seem to update.*`
-      );
-
-      //Make map based on if member is in hospital
-      factionMemberList.forEach((factionMember) => {
-        if (factionMember.status.description.includes("In hospital")) {
-          hospitalMap.set(factionMember.name, factionMember.status.until);
-        }
-      });
-
-      //If hospital list is empty
-      if (hospitalMap.size === 0) {
-        response.setTitle(`🏥 No one in hospital of ${factionName} 🏥`);
-      } else {
-        response.setTitle(`🏥 Hospital List of ${factionName} 🏥`);
-      }
-
-      //Order list so that earliest to leave hospital is above in message
-      for (const [key, value] of [...hospitalMap.entries()].sort(
-        (a, b) => a[1] - b[1]
-      )) {
-        response.addFields({
-          name: `${key}`,
-          value: `Leaving hospital <t:${value}:R>`,
-        });
-      }
-      return await interaction.guild.channels.cache
-        .get(faction.discordChannelId.toString())
-        .send({
-          embeds: [response],
-        });
-    });
-    // }, 60000);
-
-    return interaction.reply("Started Polling");
     /*
      * 1. load api keys
      * 2. do everything in a trycatch
@@ -102,3 +45,11 @@ module.exports = {
      */
   },
 };
+
+function wait(ms) {
+  var start = Date.now(),
+    now = start;
+  while (now - start < ms) {
+    now = Date.now();
+  }
+}
---
+++ src/components/functions/hospitalStatusEmbed.js
@@ -0,0 +1,71 @@
+const { EmbedBuilder } = require("discord.js");
+
+async function sendHospitalStatusEmbed(interaction, results, faction) {
+  if (results.data.error) {
+    const err = new EmbedBuilder()
+      .setColor("Aqua")
+      .setTitle(`🏥 No faction found 🏥`);
+    return await interaction.guild.channels.cache
+      .get(faction.discordChannelId.toString())
+      .send({
+        embeds: [err],
+      });
+  }
+
+  const hospitalMap = new Map();
+  //Destructure Json to array of faction members
+  const factionInfo = Object.keys(results.data);
+  let factionMemberList = undefined;
+  let factionName = undefined;
+  //Iterate over list to find out where members list is located and define variable
+  for (let i = 0; i < factionInfo.length; i++) {
+    if (factionInfo[i] === "members") {
+      factionMemberList = Object.values(Object.values(results.data)[i]);
+    }
+
+    if (factionInfo[i] === "name") {
+      factionName = Object.values(results.data)[i];
+    }
+  }
+
+  const response = new EmbedBuilder().setColor("Aqua").setDescription(
+    `${interaction.member} has asked for the the hospital list <t:${Math.round(
+      Date.now() / 1000
+    )}:R>.
+
+        **Important:**
+
+        *This list does not update on its own when someone takes medication. Also, switch channels if timestamps dont seem to update.*`
+  );
+
+  //Make map based on if member is in hospital
+  factionMemberList.forEach((factionMember) => {
+    if (factionMember.status.description.includes("In hospital")) {
+      hospitalMap.set(factionMember.name, factionMember.status.until);
+    }
+  });
+
+  //If hospital list is empty
+  if (hospitalMap.size === 0) {
+    response.setTitle(`🏥 No one in hospital of ${factionName} 🏥`);
+  } else {
+    response.setTitle(`🏥 Hospital List of ${factionName} 🏥`);
+  }
+
+  //Order list so that earliest to leave hospital is above in message
+  for (const [key, value] of [...hospitalMap.entries()].sort(
+    (a, b) => a[1] - b[1]
+  )) {
+    response.addFields({
+      name: `${key}`,
+      value: `Leaving hospital <t:${value}:R>`,
+    });
+  }
+  await interaction.guild.channels.cache
+    .get(faction.discordChannelId.toString())
+    .send({
+      embeds: [response],
+    });
+}
+
+module.exports = { sendHospitalStatusEmbed };
