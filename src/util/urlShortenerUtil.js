fd12a2d468840f7cb1e30907c6c39923c9b3aad9
Bram Janssen <ibramsterdam@hotmail.com>
2023-03-11 20:04:55 +0100

    fix bug urlShortener

--- src/util/urlShortenerUtil.js
+++ src/util/urlShortenerUtil.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const getShortUrlAttackLink = async (tornId) => {
   try {
     return axios.post(`https://xa.up.railway.app/api/generate`, {
-      url: `https://www.torn.com/loader2.php?sid=getInAttack&user2ID=$${tornId}`,
+      url: `https://www.torn.com/loader.php?sid=attack&user2ID=${tornId}`,
     });
   } catch (error) {
     return "error";
