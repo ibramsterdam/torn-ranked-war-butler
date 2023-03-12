f078f3b095f704d22f0a81af746984ed30667b38
Bram Janssen <ibramsterdam@hotmail.com>
2023-03-12 22:54:52 +0100

    remove some module.exports

--- src/commands/deleteMessages.ts
+++ src/commands/deleteMessages.ts
@@ -2,32 +2,31 @@ import { SlashCommandBuilder, CommandInteraction, Client } from "discord.js";
 import { getDiscordServer } from "../functions/prisma/discord";
 import { prisma } from "../index";
 
-module.exports = {
-  developer: true,
-  data: new SlashCommandBuilder()
-    .setName("delete-messages")
-    .setDescription("Batch deletes messages (developer only)"),
-  async execute(interaction: any, client: any) {
-    await interaction.reply("Deleting...");
-    setTimeout(async () => await interaction.deleteReply(), 5000);
+export const developer = true;
+export const data = new SlashCommandBuilder()
+  .setName("delete-messages")
+  .setDescription("Batch deletes messages (developer only)");
 
-    const guildID = BigInt(interaction.guildId);
-    const server = await getDiscordServer(prisma, guildID);
+export async function execute(interaction: any, client: any) {
+  await interaction.reply("Deleting...");
+  setTimeout(async () => await interaction.deleteReply(), 5000);
 
-    if (server) {
-      for (const faction of server.factions) {
-        const channel = await interaction.guild.channels.cache.get(
+  const guildID = BigInt(interaction.guildId);
+  const server = await getDiscordServer(prisma, guildID);
+
+  if (server) {
+    for (const faction of server.factions) {
+      const channel = await interaction.guild.channels.cache.get(
+        faction.discordChannelId.toString()
+      );
+
+      // delete all possible messages
+      await channel.bulkDelete(100, true).then(() => {
+        console.log(
+          "Messages deleted of ",
           faction.discordChannelId.toString()
         );
-
-        // delete all possible messages
-        await channel.bulkDelete(100, true).then(() => {
-          console.log(
-            "Messages deleted of ",
-            faction.discordChannelId.toString()
-          );
-        });
-      }
+      });
     }
-  },
-};
+  }
+}
