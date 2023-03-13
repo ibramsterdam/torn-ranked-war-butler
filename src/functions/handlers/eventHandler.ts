4ae10508bd828c62b2cde485dd0869abaa6fc922
Bram Janssen <ibramsterdam@hotmail.com>
2023-03-13 21:47:54 +0100

    fix: bug from typescript migration

--- src/functions/handlers/eventHandler.ts
+++ src/functions/handlers/eventHandler.ts
@@ -4,13 +4,16 @@ export async function handleEvents(client: any) {
   await client.events.clear();
 
   for (const event of Object.values(events)) {
-    const execute = (...args: any) => event.execute([...args], client);
+    //@ts-ignore
+    const execute = (...args: any) => event.execute(...args, client);
     client.events.set(event.name, execute);
 
     if (event.once) {
       client.once(event.name, (...args: any) => event.execute(client));
-    } else
-      client.on(event.name, (...args: any) => event.execute([...args], client));
+    } else {
+      //@ts-ignore
+      client.on(event.name, (...args: any) => event.execute(...args, client));
+    }
   }
 
   console.log("Events Loaded");
